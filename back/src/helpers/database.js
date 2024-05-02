13d92867aabdd356b70d8b11a54f85303664f4fd
guyevaristebade <guy.badepro@gmail.com>
2024-05-02 16:14:53 +0200

    Update de des variables d'environnements

--- back/src/helpers/database.js
+++ back/src/helpers/database.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 export const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.LOCAL_URI}`);
+        await mongoose.connect(`${process.env.MONGO_URI}`);
         console.log(`MongoDB Connected...`);
 
     } catch (err) {
