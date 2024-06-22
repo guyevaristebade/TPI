a351eefc79b2d368b8255478f93fe3062a6d2773
guyevaristebade <guy.badepro@gmail.com>
2024-06-22 23:25:51 +0200

    update

--- back/src/controllers/user.js
+++ back/src/controllers/user.js
@@ -103,6 +103,7 @@ export const isLoggedIn = (req, res, next) => {
         res.cookie('token-auth', req.cookies['token-auth'], {
             maxAge: 31 * 24 * 3600 * 1000,
             httpOnly: true,
+            sameSite : "None",
             secure: process.env.NODE_ENV !== 'development'
         });
     }
