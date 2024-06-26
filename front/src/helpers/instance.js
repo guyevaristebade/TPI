e0d59b088d976fd4e2ee21634f1d958ae06c9e70
guyevaristebade <guy.badepro@gmail.com>
2024-06-26 22:41:15 +0200

    je souffre avec les cookies

--- front/src/helpers/instance.js
+++ front/src/helpers/instance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const instance = axios.create({
   //baseURL : 'http://localhost:2024/api',
-  baseURL : 'https://tpi-hazel.vercel.app/api',
+  baseURL : 'https://ataliantsecurityback.vercel.app/api',
   withCredentials : true,
   headers : { 'Content-Type' : 'application/json'}
 })
