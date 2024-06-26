189bc57792fb8430962849049688c66abc28a23f
guyevaristebade <guy.badepro@gmail.com>
2024-06-26 21:50:58 +0200

    je souffre avec les cookies

--- front/src/helpers/instance.js
+++ front/src/helpers/instance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const instance = axios.create({
   //baseURL : 'http://localhost:2024/api',
-  baseURL : 'https://agentequipement.onrender.com/api',
+  baseURL : 'https://ataliansecurityback.vercel.app/api',
   withCredentials : true,
   headers : { 'Content-Type' : 'application/json'}
 })
