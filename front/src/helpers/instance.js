e114300374f408a2556771de9bab7ea712b7543d
guyevaristebade <guy.badepro@gmail.com>
2024-08-10 15:35:59 +0200

    update du code pour le login, car la connexion ne fonctionne pas

--- front/src/helpers/instance.js
+++ front/src/helpers/instance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL : 'http://localhost:2024/api',
-  //baseURL : 'https://ataliantsecurityback.vercel.app/api',
+  //baseURL : 'http://localhost:2024/api',
+  baseURL : 'https://ataliantsecurityback.vercel.app/api',
   withCredentials : true,
   headers : {
     'Content-Type': 'application/json'
